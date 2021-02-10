daab7751b0f623478a12bcfe72622b881bdd5e86
Samuel George <samuegeorge58@gmail.com>
2021-02-10 03:11:22 +0100

    fix linting

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import ProductCard from "../ProductCard/ProductCard";
 import Loader from "../Loader/Loader";
