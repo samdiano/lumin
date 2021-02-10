0ab59ed55744ec19aadd35e27313b9b70df52c09
Samuel George <samuegeorge58@gmail.com>
2021-02-10 03:04:23 +0100

    add products list page and product card

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// loader style
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+    </div>
+  )
+}
+
+export default Loader;
